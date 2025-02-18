c6758c1953284ec9483e8f1f922683e133905af9
unknown <arifk62901@gmail.com>
2025-02-18 17:28:07 +0600

    responsive for all device

--- src/AdminDashboard/AdminDashboard.jsx
+++ src/AdminDashboard/AdminDashboard.jsx
@@ -4,6 +4,7 @@ import { useQuery } from '@tanstack/react-query';
 import useAxiosSecure from '../Hook/useAxiosSecure';
 import { FaUtensils, FaShoppingCart, FaUsers, FaMoneyBillWave } from 'react-icons/fa';
 
+
 export default function AdminDashboard() {
   const { user } = useAuth();
   const axiosSecure = useAxiosSecure();
@@ -16,13 +17,15 @@ export default function AdminDashboard() {
   });
 
   return (
+    <div>
+
     <div className="p-6 space-y-6 min-h-screen text-white">
       <div className="text-start text-sky-900">
         <h1 className='text-3xl font-bold'>Hi, Welcome {user?.displayName || "Back"}!</h1>
       </div>
       
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
-        <div className="stat shadow-lg p-4 bg-gradient-to-r from-yellow-500 to-yellow-700 rounded-lg border border-gray-300 flex items-center">
+        <div className="stat shadow-lg p-4 bg-gradient-to-r from-yellow-500 to-yellow-400 rounded-lg border border-gray-300 flex items-center">
           <FaUtensils className="text-white text-4xl mr-4" />
           <div>
             <div className="stat-title text-white text-lg font-semibold"> Meal</div>
@@ -31,7 +34,7 @@ export default function AdminDashboard() {
           </div>
         </div>
         
-        <div className="stat shadow-lg p-4 bg-gradient-to-r from-green-500 to-green-700 rounded-lg border border-gray-300 flex items-center">
+        <div className="stat shadow-lg p-4 bg-gradient-to-r from-green-500 to-green-400 rounded-lg border border-gray-300 flex items-center">
           <FaShoppingCart className="text-white text-4xl mr-4" />
           <div>
             <div className="stat-title text-white text-lg font-semibold"> Meal Request</div>
@@ -40,7 +43,7 @@ export default function AdminDashboard() {
           </div>
         </div>
         
-        <div className="stat shadow-lg p-4 bg-gradient-to-r from-blue-500 to-blue-700 rounded-lg border border-gray-300 flex items-center">
+        <div className="stat shadow-lg p-4 bg-gradient-to-r from-blue-500 to-blue-400 rounded-lg border border-gray-300 flex items-center">
           <FaUsers className="text-white text-4xl mr-4" />
           <div>
             <div className="stat-title text-white text-lg font-semibold"> Users</div>
@@ -49,7 +52,7 @@ export default function AdminDashboard() {
           </div>
         </div>
         
-        <div className="stat shadow-lg p-4 bg-gradient-to-r from-red-500 to-red-700 rounded-lg border border-gray-300 flex items-center">
+        <div className="stat shadow-lg p-4 bg-gradient-to-r from-red-500 to-red-400 rounded-lg border border-gray-300 flex items-center">
           <FaMoneyBillWave className="text-white text-4xl mr-4" />
           <div>
             <div className="stat-title text-white text-lg font-semibold"> Revenue</div>
@@ -58,6 +61,8 @@ export default function AdminDashboard() {
           </div>
         </div>
       </div>
+    </div>
+    
     </div>
   );
 }
